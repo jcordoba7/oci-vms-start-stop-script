b1e0ff96ff3f8df89c2061cf0b3a3740c0f5291d
Jonathan Córdoba <j.cordoba1021@gmail.com>
2023-09-05 09:58:13 -0500

    Create script-start.sh
    First version for script-start file.

---
+++ script-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "====== Searching for servers ======"
+
+# Searching for resources with tag "free-form.shutdown:true"
+resources=$(oci search resource structured-search --query-text "query instance resources where (freeformTags.key = 'shutdown' && freeformTags.value = 'true')" --raw-output)
+
+# Go through the list of resources and turn them on
+for resource in $(echo "${resources}" | jq -r '.data.items[].identifier')
+do
+    echo "Encendiendo recurso: ${resource}"
+    oci compute instance action --action start --instance-id ${resource}
+done
