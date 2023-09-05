4135aeb83ba633badf457ce44f7957ec3c01e620
Jonathan Córdoba <j.cordoba1021@gmail.com>
2023-09-05 10:05:27 -0500

    Update script-start.sh
    Adjust in the commented lines.

--- script-start.sh
+++ script-start.sh
@@ -8,6 +8,6 @@ resources=$(oci search resource structured-search --query-text "query instance r
 # Go through the list of resources and turn them on
 for resource in $(echo "${resources}" | jq -r '.data.items[].identifier')
 do
-    echo "Encendiendo recurso: ${resource}"
+    echo "Starting resource: ${resource}"
     oci compute instance action --action start --instance-id ${resource}
 done
